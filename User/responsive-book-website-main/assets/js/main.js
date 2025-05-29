b646c5e959232392714bbaf7abf9faf949e0db0e
Carlos Charles <sengicarloscharles@gmail.com>
2025-05-29 03:40:56 +0300

    #7

--- User/responsive-book-website-main/assets/js/main.js
+++ User/responsive-book-website-main/assets/js/main.js
@@ -126,6 +126,37 @@ window.addEventListener('scroll', scrollUp);
 
 // ===========chatbot==================
 
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+
+const createChatLi  = (message, className) =>{
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ?`<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    return chatLi;
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+
+    setTimeout(() => {
+
+    chatbox.appendChild(createChatLi("Thinking...", "incoming"));
+    generateResponse();
+
+},600);
+}
+
+
+sendChatBtn.addEventListener("click",handleChat);
+
 const chatbotToggle = document.getElementById('chatbot-toggle');
 const chatbotContent = document.getElementById('chatbot-content');
 
